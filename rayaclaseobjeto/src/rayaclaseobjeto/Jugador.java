8a1de84bc7d20cf0c9b7ad0e347967b842db5bd4
Ahidlop <145803894+Ahidlop@users.noreply.github.com>
2024-04-26 12:55:41 +0200

    Comprobar filas correcto!!

---
+++ rayaclaseobjeto/src/rayaclaseobjeto/Jugador.java
@@ -0,0 +1,47 @@
+
+package rayaclaseobjeto;
+
+import java.util.Scanner;
+
+public class Jugador {
+    private char fichaActual;
+    private String nombre;
+    private int turno;
+    
+    //Constructor
+    public Jugador( String nombre, char fichaActual, int turno){
+        this.fichaActual=fichaActual;
+        this.nombre=nombre;
+        this.turno=turno;
+    }
+
+    public char getFicha() {
+        return fichaActual;
+    }
+
+    public void setFicha(char fichaActual) {
+        this.fichaActual = fichaActual;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTurno() {
+        return turno;
+    }    
+    
+    public void setTurno(int turno) {
+        this.turno = turno;
+    } 
+    
+    public void imprimirJugadores(){
+        System.out.println("Nombre: "+ nombre);
+        System.out.println("Ficha: "+fichaActual);
+        System.out.println("Turno: "+turno);
+    }
+}
---
+++ rayaclaseobjeto/src/rayaclaseobjeto/Rayaclaseobjeto.java
@@ -0,0 +1,44 @@
+package rayaclaseobjeto;
+
+import java.util.Scanner;
+
+public class Rayaclaseobjeto {
+
+    public static void main(String[] args) {
+        LogicaJuego j1 = new LogicaJuego ();
+        
+        /*Scanner leer = new Scanner (System.in);
+        char jug1='X', jug2='Y';
+        String J1="" , J2="";
+        int empezar, n;
+        boolean win=false;
+        int contador =0, revancha;
+    
+        //INICIO
+    System.out.println("Jugador X: Introduzca su nombre");
+    J1=leer.nextLine();
+    System.out.println("Jugador Y: Introduzca su nombre");
+    J2=leer.nextLine();
+    System.out.println("¿A qué 'n' en raya deseas jugar?");
+    n=leer.nextInt();
+    char tablero [][]=new char [n][n];
+    turno=jugadores( turno,  J1,  J2, leer);
+    vacioTablero(tablero);
+    
+    while (!win)
+        {         
+            turno=rellenarTablero(jug1,jug2,tablero,turno,leer,J1,J2);
+            contador++;
+            imprimirTablero(tablero);
+            if (contador>2){
+            win=comprobar (tablero,win,J1,J2);
+            }
+            win=revancha (tablero, win, leer );
+            
+        } 
+    }*/
+        
+        
+    }
+    
+}
